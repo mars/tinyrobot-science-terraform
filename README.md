def894f69ff3ecd06687bb92dcbc08f0d28d851b
Mars Hall <mars@heroku.com>
2018-07-27 13:15:57 -0700

    Initial dev state config

---
+++ README.md
@@ -0,0 +1,4 @@
+# Terraforming 🌱→🤖🔬 tinyrobot.science
+
+*Based on [Confluent's Terraform config with S3 state storage](https://github.com/confluentinc/terraform-state-s3).*
+
---
+++ environments/dev-tfstate/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 1.29"
+}
+
+module "dev-tfstate" {
+  source = "git@github.com:mars/terraform-state-s3.git"
+  env = "dev"
+  s3_bucket = "tinyrobot-science-terraform-dev"
+  s3_bucket_name = "Terraform State Store"
+  dynamodb_table = "tinyrobot_science_terraform_dev"
+}
