b1b72df2c74384ab9cec2227ea15cfa8ea382bfc
Mars Hall <mars@heroku.com>
2018-09-18 14:30:02 -0700

    format code

--- environments/dev-tfstate/main.tf
+++ environments/dev-tfstate/main.tf
@@ -4,9 +4,9 @@ provider "aws" {
 }
 
 module "dev-tfstate" {
-  source = "git@github.com:mars/terraform-state-s3.git"
-  env = "dev"
-  s3_bucket = "tinyrobot-science-terraform-dev"
+  source         = "git@github.com:mars/terraform-state-s3.git"
+  env            = "dev"
+  s3_bucket      = "tinyrobot-science-terraform-dev"
   s3_bucket_name = "Terraform State Store"
   dynamodb_table = "tinyrobot_science_terraform_dev"
 }
